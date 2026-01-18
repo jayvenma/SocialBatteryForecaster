b7b2b7a684d2a7b6f75d33097d1f19cf9c8fb8d7
jayvenma <masonjayven@gmail.com>
2026-01-18 05:38:06 -0500

    debugging

--- backend/app/main.py
+++ backend/app/main.py
@@ -179,13 +179,14 @@ app.add_middleware(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://socialbatteryforecaster.xyz",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+    )
 
 
 # ------------------
